3384931d119cd7f28bcf45b88efd3780adc2bfc1
Talaron18 <mainauctor@gmail.com>
2026-02-15 19:39:03 +0800

    modified:   whisper/install.py

--- whisper/install.py
+++ whisper/install.py
@@ -1,7 +1,12 @@
 from optimum.exporters.openvino.convert import export_tokenizer
 from optimum.intel import OVModelForSpeechSeq2Seq
 from transformers import AutoTokenizer
-output_dir="C:\\Users\\32873\\.vscode\\whisper\\whisper-base"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+output_dir=os.environ.get("DIR")
 model=OVModelForSpeechSeq2Seq.from_pretrained("openai/whisper-base", export=True, trust_remote_code=True)
 model.save_pretrained(output_dir)
 
