8853d99d366f4e1b18c3b97e1eed9083830cc26e
Talaron18 <mainauctor@gmail.com>
2026-02-16 10:08:52 +0800

    modified:   whisper/tests/wake_word.py

--- whisper/tests/wake_word.py
+++ whisper/tests/wake_word.py
@@ -1,6 +1,8 @@
 import pyaudio
-from ..stream.utils import wake
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+from AssistantGlasses.whisper.stream.utils import wake
 from dotenv import load_dotenv
 import pvporcupine
 
@@ -15,11 +17,11 @@ def stream():
     # check out for output devices
     for i in range(pa.get_device_count()):
         print(pa.get_device_info_by_index(i))
-    input("press Enter to continue...")
+    input("Press Enter to continue...")
     print("\n")
     # check out for input devices
     print(pa.get_default_input_device_info())
-    input("press Enter to continue")
+    input("Press Enter to continue")
     streaming=pa.open(
         input_device_index=1, # match the right input device
         channels=1, # porcupine requires single channel input
@@ -29,15 +31,15 @@ def stream():
         frames_per_buffer=audio.frame_length
     )
     if streaming !=None:
-        print("system initiated...")
+        print("System initiated...")
     try:
         while True:
             audio_data=streaming.read(audio.frame_length,exception_on_overflow=False)
-            # speek to microphone to test the 
+            # speek to microphone to test wake word
             if wake(audio,audio_data):
                 print("On your command")
     except:
-        print("stop recording...")
+        print("Stop recording...")
     finally:
         # clear cache occupancy
         streaming.stop_stream()
